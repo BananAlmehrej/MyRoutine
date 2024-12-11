aae29106a75ac855c684073f79b2a2e75b20173c
BananAlmehrej <bananalmehrej@gmail.com>
2024-12-11 09:46:07 +0300

    Initial Commit

---
+++ MyRoutine/MyRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRoutineApp.swift
+//  MyRoutine
+//
+//  Created by Banan Almehrej on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyRoutineTests/MyRoutineTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyRoutineTests.swift
+//  MyRoutineTests
+//
+//  Created by Banan Almehrej on 11/12/2024.
+//
+
+import Testing
+@testable import MyRoutine
+
+struct MyRoutineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
